fed23786da1f48231aea0051a98b7964d87d6120
Rustem <samoilenkorustem@gmail.com>
2026-01-25 22:44:45 +0200

    a

--- tests/AUHealth/auHealthFast.spec.ts
+++ tests/AUHealth/auHealthFast.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from "../../src/fixtures/testFixture";
 
 test.describe('AU health check', () => {
-    test('Check AU health fast', async ({ kingBilly }) => {
+    test('Check AU health fast',{tag: '@fast'}, async ({ kingBilly }) => {
         await kingBilly.mainPage.navTo('/');
         await expect(kingBilly.mainPage.header.getSignInButton).toBeVisible();
     });
